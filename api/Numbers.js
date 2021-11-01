1eeb4d4e2551d022b88dbb2adb926c66cb3a20aa
wellpinho <wellpinho@outlook.com>
2021-11-01 09:53:36 -0300

    NEW FEATURE: map with numbers

---
+++ api/Numbers.js
@@ -0,0 +1,3 @@
+const Numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+module.exports = Numbers
--- map.js
+++ map.js
@@ -1,9 +1,13 @@
-// COM Map() PODEMOS CRIAR NOVOS ARRAYS DO MESMO TAMANHO
-// MAS FAZENDO ALTERAÇÕES E RETORNANDO O QUE QUISER
-// ARRAY SÓ DE ids, SÓ COM NOMES E ETC.
+/*
+*  QUANDO DEVMOS USAR O MAP?
+*  Quando queremos um novo array com a mes quantidade,
+*  mas que cada item seja transformado.
+*/
 
+const Numbers = require("./api/Numbers");
 const Products = require("./api/Products");
 
+// EXEMPLO 01
 // const prodId = Products.map(product => product.id)
 // const prodName = Products.map(product => product.name)
 // const prodPrice = Products.map(product => product.price)
@@ -16,9 +20,27 @@ const Products = require("./api/Products");
 //   Categoria: ${prodCategory}
 // `)
 
-const addPercentToPrice = Products.map(product => {
-  const percent = (product.price / 10) + product.price
-  return `${product.name}: ${percent.toFixed(2)}`
-})
+//EXEMPLO 02
+// const addPercentToPrice = Products.map(product => {
+//   const percent = (product.price / 10) + product.price
+//   return `${product.name}: ${percent.toFixed(2)}`
+// })
 
-console.log(addPercentToPrice)
+// console.log(addPercentToPrice)
+
+// EXEMPLO 03
+const result = Numbers.map(number => number)
+const sumAfter = Numbers.map(number => number + 1)
+const mult = Numbers.map(number => number * 2)
+const division = Numbers.map(number => number / 2)
+const restOfDivision = Numbers.map(number => number % 2 === 0)
+const squareRoot = Numbers.map(number => Math.sqrt(number).toFixed(2))
+
+console.log(`
+  Resultado original: ${result}
+  Soma + 1: ${sumAfter}
+  Multplicação: ${mult}
+  Divisão: ${division}
+  Resto de uma divisão: ${restOfDivision}
+  Raiz quadrada: ${squareRoot}
+`)
